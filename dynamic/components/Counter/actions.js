793b74a98706e50303592f4fe9243042a91cee97
James Hunter <jamesb@everydayhero.com.au>
2015-01-18 12:00:59 +1100

    Update actions.js

--- dynamic/components/Counter/actions.js
+++ dynamic/components/Counter/actions.js
@@ -4,12 +4,11 @@ var send = require("oro-dispatcher/lib/send");
 var post = require("oro-xhr/lib/post");
 
 function postInc(id) { return post("/api/v1/counter/inc", {id}); }
-function postIncSuccess(res) { send("COUNTERS_UPDATE", {data: JSON.parse(res.text)}); }
-
 function postDec(id) { return post("/api/v1/counter/dec", {id}); }
-function postDecSuccess(res) { send("COUNTERS_UPDATE", {data: JSON.parse(res.text)}); }
+
+function updateCounters(res) { send("COUNTERS_UPDATE", {data: JSON.parse(res.text)}); }
 
 module.exports = {
-  inc(id) { postInc(id).then(postIncSuccess); },
-  dec(id) { postDec(id).then(postDecSuccess); }
+  inc(id) { postInc(id).then(updateCounters); },
+  dec(id) { postDec(id).then(updateCounters); }
 };
